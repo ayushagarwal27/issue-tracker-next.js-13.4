7a5624c9d09ad4f14b4ee8c2100c0a21c59b43a2
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-11-14 16:58:02 +0530

    Refactor: Apply the SRP

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,21 @@
+import { IssueStatusBadge } from '@/app/components';
+import { Issue } from '@prisma/client';
+import { Card, Flex, Heading, Text } from '@radix-ui/themes';
+import ReactMarkdown from 'react-markdown';
+
+const IssueDetails = ({ issueDetail }: { issueDetail: Issue }) => {
+  return (
+    <>
+      <Heading className='capitalize'>{issueDetail.title}</Heading>
+      <Flex gap='3' my={'2'}>
+        <IssueStatusBadge status={issueDetail.status} />
+        <Text>{issueDetail.created_at.toDateString()}</Text>
+      </Flex>
+      <Card className='prose' mt='4'>
+        <ReactMarkdown>{issueDetail.description}</ReactMarkdown>
+      </Card>
+    </>
+  );
+};
+
+export default IssueDetails;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,11 +1,9 @@
-import IssueStatusBadge from '@/app/components/IssueStatusBadge';
-import prisma from '@/prisma/client';
-import { Box, Button, Card, Flex, Grid, Heading, Text } from '@radix-ui/themes';
-import { notFound } from 'next/navigation';
 import { FC } from 'react';
-import ReactMarkdown from 'react-markdown';
-import { Pencil2Icon } from '@radix-ui/react-icons';
-import Link from 'next/link';
+import prisma from '@/prisma/client';
+import { Box, Grid } from '@radix-ui/themes';
+import { notFound } from 'next/navigation';
+import EditIssueButton from './EditIssueButton';
+import IssueDetails from './IssueDetails';
 
 interface IssueDetailPageProps {
   params: { id: string };
@@ -14,31 +12,21 @@ interface IssueDetailPageProps {
 const IssueDetailPage: FC<IssueDetailPageProps> = async ({
   params: { id },
 }) => {
-  const issueDetail = await prisma.issue.findUnique({
+  const issue = await prisma.issue.findUnique({
     where: { id: parseInt(id) },
   });
 
-  if (!issueDetail) {
+  if (!issue) {
     notFound();
   }
 
   return (
     <Grid columns={{ initial: '1', md: '2' }} gap='5'>
       <Box>
-        <Heading className='capitalize'>{issueDetail.title}</Heading>
-        <Flex gap='3' my={'2'}>
-          <IssueStatusBadge status={issueDetail.status} />
-          <Text>{issueDetail.created_at.toDateString()}</Text>
-        </Flex>
-        <Card className='prose' mt='4'>
-          <ReactMarkdown>{issueDetail.description}</ReactMarkdown>
-        </Card>
+        <IssueDetails issueDetail={issue} />
       </Box>
       <Box>
-        <Button>
-          <Pencil2Icon />
-          <Link href={`/issues/${issueDetail.id}/edit`}>Edit Issue</Link>
-        </Button>
+        <EditIssueButton issueId={issue.id} />
       </Box>
     </Grid>
   );
