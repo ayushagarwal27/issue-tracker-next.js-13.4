9cdc51f011adbdff8991969631cff99902153424
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-11-17 23:07:37 +0530

    Show a confirmation dialog box

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,7 +4,7 @@ import { Box, Flex, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
 import EditIssueButton from './EditIssueButton';
 import IssueDetails from './IssueDetails';
-import DeleteIssueButton from './DeleteissueButton';
+import DeleteIssueButton from './DeleteIssueButton';
 
 interface IssueDetailPageProps {
   params: { id: string };
