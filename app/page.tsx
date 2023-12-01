a94fcc06c20c5b34dbfb6267d82bccb367b37194
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-12-01 20:09:00 +0530

    Build the issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import LatesIssues from './LatesIssues';
+import prisma from '@/prisma/client';
+import IssueSummary from './IssueSummary';
 
-export default function Home() {
-  return <LatesIssues />;
+export default async function Home() {
+  const openCount = await prisma.issue.count({ where: { status: 'OPEN' } });
+  const inProgressCount = await prisma.issue.count({ where: { status: 'IN_PROGRESS' } });
+  const closedIssuesCount = await prisma.issue.count({ where: { status: 'CLOSED' } });
+
+  return <IssueSummary open={openCount} inProgress={inProgressCount} closed={closedIssuesCount} />;
 }
